f268348ca4f73f4f3d0fc6d5e7dae0d0752b6977
Fabrice HOUNTONDJI <fabricehoutondji@gmail.com>
2026-03-07 03:42:11 -0500

    base

--- docs/eslint.config.js
+++ docs/eslint.config.js
@@ -28,14 +28,4 @@ export default defineConfig([
   },
 ])
 
-// export default {
-//   content: [
-//     "./index.html",
-//     "./src/**/*.{js,jsx,ts,tsx}"
-//   ],
-//   theme: {
-//     extend: {}
-//   },
-//   plugins: []
-// };
 
